dcd0094df2bbb19ca9f599164d6c2e9d50f25cf7
LUGIA <79367636+LSZTM@users.noreply.github.com>
2022-02-19 09:11:20 +0530

    new

---
+++ src/This_keyword.java
@@ -0,0 +1,22 @@
+class This_keyword{
+
+
+
+
+   int a = 20;
+    public static void main(String[] args){
+     This_keyword b = new This_keyword();
+     b.This_keyword();
+
+
+
+    }
+    void This_keyword(){
+     int a = 10;
+     System.out.println(a);
+     System.out.print(this.a);
+
+
+
+    }
+}
